25ef26784f2fa0e2f5610e7169970dfc83a17fc7
manubett254 <128588373+manubett254@users.noreply.github.com>
2024-02-28 11:35:54 +0300

    user input assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# taking user inputs
+name=input("What is your name?")
+age=input("How old are you?")
+location=input("Where do you live?")
+# printing user 
+print("hello",name,"you are",age,"years old and you live in",location)
+
